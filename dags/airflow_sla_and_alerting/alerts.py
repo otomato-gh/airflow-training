dd9fe771b5069b8f1348012cc45881ae31280954
Ant Weiss <anton@otomato.io>
2023-05-03 05:44:04 +0000

    slack

--- dags/airflow_sla_and_alerting/alerts.py
+++ dags/airflow_sla_and_alerting/alerts.py
@@ -5,25 +5,25 @@ class AlertManager:
 
     @staticmethod
     def on_failure_callback(context):
-        text = ':red_circle: %s task failed' % str(context['task_instance'])
+        text = ':large_red_circle: %s task failed' % str(context['task_instance'])
         AlertManager.post(text=text)
 
     @staticmethod
     def on_retry_callback(context):
-        text = ':blue_circle: %s task retry' % str(context['task_instance'])
+        text = ':large_blue_circle: %s task retry' % str(context['task_instance'])
         AlertManager.post(text=text)
 
     @staticmethod
     def on_success_callback(context):
-        text = ':green_circle: %s task success' % str(context['task_instance'])
+        text = ':large_green_circle: %s task success' % str(context['task_instance'])
         AlertManager.post(text=text)
 
     @staticmethod
     def sla_miss_callback(context):
-        text = ':red_circle: %s sla miss' % str(context['task_instance'])
+        text = ':large_red_circle: %s sla miss' % str(context['task_instance'])
         AlertManager.post(text=text)
 
     @staticmethod
     def post(text):
-        hook = SlackWebhookImpl(text=text)
+        hook = SlackWebhookImpl(text=text, url='...')
         hook.execute()
